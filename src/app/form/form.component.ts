a9daa87d261067b23dea243baa3daf8e227f9ef2
tuyenpv02 <tuyenphamk202@gmail.com>
2023-11-19 14:20:30 +0700

    crud form user

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -9,37 +9,57 @@ import { NgForm } from '@angular/forms';
 export class FormComponent {
 
   inputUser = {
+    id:0,
     name:'tuyend',
     age:22
   }
   users=[
-    {id:1, name:'tuyen', age:0},
-    {id:10, name:'tú', age:20},
+    {id:1, name:'tuyen', age:7},
+    {id:10, name:'văn tú', age:20},
   ]
 
-  onSubmit(userForm: NgForm){
-    // lấy id max
-    const newUsersId = this.users
-    .map(u => u.id) // lấy ra mảng mới chỉ có id
-    .sort((a, b)=> b - a); // sort id đag có theo chiều giảm dần
-    const idMax = newUsersId[0];
-    //push dữ liệu vào lst
-    this.users.push({
-      ...userForm.value // lấy tất cả dữ liệu của formData và ở dưới thêm id
-      ,id:idMax +1
-    });
+  onSubmit(userForm: NgForm, key:string){
+    if(!userForm.valid){
+      alert('thiếu dữ liệu');
+      return;
+    }
+    if(key =='add'){
+       // lấy id max
+      const newUsersId = this.users
+      .map(u => u.id) // lấy ra mảng mới chỉ có id
+      .sort((a, b)=> b - a); // sort id đag có theo chiều giảm dần
+      const idMax = newUsersId[0];
+      //push dữ liệu vào lst
+      this.users.push({
+        ...userForm.value // lấy tất cả dữ liệu của formData và ở dưới thêm id
+        ,id:idMax +1
+      });
+      // cập nhật lại form
+      userForm.resetForm({ // cập nhật form về giá trị default
+          name:'',
+          age:0,
+          id:0
+        });
+    }
+   if(key =='update' && userForm.value.id != 0){
+    console.log(22);
     
-    // cập nhật lại form 
-    // userForm.reset(); //(reset form
-    userForm.resetForm({ // cập nhật form về giá trị default
-        name:'',
-        age:0
-      }
-    )
-
+    const x = this.users.findIndex((u)=>u.id == userForm.value.id);
+    this.users[x] = userForm.value;
+    userForm.resetForm({
+      name:'',
+      age:0,
+      id:0,
+    });
+   }
   }
 
   deleteUser(id:number){
     this.users.splice(id, 1);
   }
+
+  editUser(id:number){
+    this.inputUser = this.users[id];
+  }
+
 }
